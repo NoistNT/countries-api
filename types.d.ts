5458d1476b0cd421c6ba36e3f7ae58b046a414af
NoistNT <arielgnr23@gmail.com>
2024-03-25 20:48:30 -0300

    Add types

---
+++ types.d.ts
@@ -0,0 +1,59 @@
+export interface Name {
+  common: string;
+  official: string;
+  nativeName?: { [languageCode: string]: { official: string; common: string } };
+}
+
+export interface Currency {
+  name: string;
+  symbol: string;
+}
+
+export interface Translation {
+  official: string;
+  common: string;
+}
+
+export interface Demonyms {
+  f: string;
+  m: string;
+}
+
+export interface Car {
+  signs: string[];
+  side: 'left' | 'right';
+}
+
+export interface Country {
+  name: Name;
+  tld: string[];
+  cca2: string;
+  ccn3: string;
+  cca3: string;
+  cioc: string;
+  independent: boolean;
+  status: string;
+  unMember: boolean;
+  currencies: { [currencyCode: string]: Currency };
+  idd: { root: string; suffixes: string[] };
+  capital: string[];
+  altSpellings: string[];
+  region: string;
+  subregion: string;
+  languages: { [languageCode: string]: string };
+  translations: { [languageCode: string]: Translation };
+  latlng: number[];
+  landlocked: boolean;
+  borders: string[];
+  area: number;
+  demonyms: { [demonymsCode: string]: Demonyms };
+  flag: string;
+  maps: { googleMaps: string; openStreetMaps: string };
+  population: number;
+  gini: { [year: string]: number };
+  fifa: string;
+  car: Car;
+  timezones: string[];
+  continents: string[];
+  flags: string[];
+}
