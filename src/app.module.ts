9ae47905e332a129a5fa3c1a7fe88cc0baaef818
NoistNT <arielgnr23@gmail.com>
2024-03-25 20:48:14 -0300

    Connect to mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { SeederModule } from './seeder/seeder.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    SeederModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
